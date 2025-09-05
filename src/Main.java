d4a7456ce346075c03bd05b6a8f9c6bb12935033
KoleSchramm <kole.schramm@csuglobal.edu>
2025-09-05 15:07:32 -0500

    Finished Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        PostfixCalculator postfixCalculator = new PostfixCalculator();
+        System.out.println(postfixCalculator.calculate("3.5 10.8 *"));
     }
 }
--- src/PostfixCalculator.java
+++ src/PostfixCalculator.java
@@ -1,2 +1,47 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class PostfixCalculator {
+    public double calculate(String string) {
+        String element;
+        Scanner scan = new Scanner(string);
+        Stack<Double> stack = new Stack<>();
+
+        try {
+            while (scan.hasNext()) {
+                element = scan.next();
+
+                try {
+                    stack.push(Double.parseDouble(element));
+                } catch (Exception e) {
+                    if (element.equals("+")) {
+                        stack.push(stack.pop() + stack.pop());
+                    }
+                    if (element.equals("-")) {
+                        stack.push(stack.pop() - stack.pop());
+                    }
+                    if (element.equals("*")) {
+                        stack.push(stack.pop() * stack.pop());
+                    }
+                    if (element.equals("/")) {
+                        stack.push(stack.pop() / stack.pop());
+                    }
+                    if (element.equals("%")) {
+                        stack.push(stack.pop() % stack.pop());
+                    }
+                }
+            }
+            if(stack.size() == 1) {
+                return stack.peek();
+            }
+            else{
+                System.out.println("Invalid input.");
+                return 0;
+            }
+        }
+        catch (Exception e){
+            System.out.println("Invalid input.");
+            return 0;
+        }
+    }
 }
